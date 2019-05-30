b3b08b7ce869b0083fe509a8db5ce836a5a5f9d1
vallyugo <vallydike@gmail.com>
2019-05-30 23:55:39 +0100

    README.md

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # job-list-platform
+INTRODUCTION
+The job-list-platform displays a plateform that connects job seekers to jobs. Employers signup and registers vacancy in their organization. The information from employers are used to set up the plateform form for interested and qualified Job seekers. In this arrangement, Employers and Job seekers could have a common ground.
+
+REQUIREMENTS
+In this project, npm jest was install. Ajax method was used to fetch database information using JSON server.
+
+
+CONFIGURATION
+Initialize npm and as well install jest. JSON server should be install 
+to start up json server through the terminal
+json-server --watch db.jon      ...........(db.json is the sever name)
--- main.js
+++ main.js
@@ -23,6 +23,32 @@ $(document).ready(function () {
         e.preventDefault();
     })
 
+    $("#searchlist").click(function () {
+        var P = $("#sellectfunction").val();
+
+        $.ajax({
+            type: "GET",
+            url: "http://localhost:3000/Employer/?q=" + P,
+            success: function (data) {
+                $('#appear').text('');
+                for (i = 0; i < data.length; i++) {
+
+                    word = "";
+                    word += "Company: ";
+                    word += data[i].Company + "<br>";
+                    word += "Industry: "
+                    word += data[i].Industry + "<br>";
+                    word += "Job Function: ";
+                    word += data[i]["Job function"] + "<br>";
+                    word += "Location: ";
+                    word += data[i].location + "<br>" + "<br>";
+                    $("#appear").append(word);
+
+                }
+
+            }
+        })
+    })
 
     $("#searchlist").click(function () {
         var A = $("#sellectfunction").val();
@@ -33,6 +59,7 @@ $(document).ready(function () {
                 type: "GET",
                 url: "http://localhost:3000/Employer",
                 success: function (data) {
+                    $('#appear').text('');
                     for (i = 0; i < data.length; i++) {
                         word = "";
                         word += "Company: ";
@@ -53,6 +80,7 @@ $(document).ready(function () {
             })
         }
     })
+
 
 });
 
